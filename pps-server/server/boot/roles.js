2349c93f03d466c935b45944ecc6b8d6980561a9
Asim Siddiqui <asim.siddiqui@us.ibm.com>
2017-07-20 11:24:15 -0700

    version for APIC testing

--- pps-server/server/boot/roles.js
+++ pps-server/server/boot/roles.js
@@ -1,41 +1,41 @@
-//module.exports = function (app) {
-//  var async = require('async');
-//
-//  var Role = app.models.Role;
-//  var RoleMapping = app.models.RoleMapping;
-//
-//  var roleInSystem = [
-//    'TerritoryManager',
-//    'SalesManager',
-//    'AccountExecutive',
-//    'Admin',
-//    'sa'
-//  ];
-//
-//  async.forEach(roleInSystem, function (role, cb) {
-//    Role.find({where: {name: role}}, function (err, foundRole) {
-//      if (err) {
-//        return cb(err);
-//      }
-//
-//      if (foundRole && foundRole.length > 0) {
-//        return cb();
-//      }
-//
-//      Role.create({name: role}, function (err, createdRole) {
-//        if (err) {
-//          return cb(err);
-//        }
-//
-//        console.log('Roles: Role %s created', role);
-//
-//        cb();
-//      });
-//    });
-//  }, function (err) {
-//    if (err) {
-//      return console.log(err);
-//    }
-//  });
-//};
+module.exports = function (app) {
+ var async = require('async');
+
+ var Role = app.models.Role;
+ var RoleMapping = app.models.RoleMapping;
+
+ var roleInSystem = [
+   'TerritoryManager',
+   'SalesManager',
+   'AccountExecutive',
+   'Admin',
+   'sa'
+ ];
+
+ async.forEach(roleInSystem, function (role, cb) {
+   Role.find({where: {name: role}}, function (err, foundRole) {
+     if (err) {
+       return cb(err);
+     }
+
+     if (foundRole && foundRole.length > 0) {
+       return cb();
+     }
+
+     Role.create({name: role}, function (err, createdRole) {
+       if (err) {
+         return cb(err);
+       }
+
+       console.log('Roles: Role %s created', role);
+
+       cb();
+     });
+   });
+ }, function (err) {
+   if (err) {
+     return console.log(err);
+   }
+ });
+};
 
